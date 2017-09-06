1dfa597a08565554e6b12323b2a3870783fda424
Bably Kumari and Megan Olson <megan@>
2017-09-06 13:53:33 -0700

    initialize game in console

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,35 +1,42 @@
+require('pry')
 module Turn
-  options = ["rock", "paper", "scissors"]
   def player_turn
     "Your turn!"
     puts "What is your choice?"
     user_input = gets.chomp.downcase
   end
   def computer_turn
+    options = ["rock", "paper", "scissors"]
     "Computer turn!"
     computer_choice = options[rand(options.length)]
+    puts "computer choose " + computer_choice
   end
 end
 
 class RPS
   include Turn
+
   def wins?(user_input, computer_choice)
     if (user_input == "rock" && computer_choice == "scissors")
-      return true
       puts "You win!"
-    elsif (user_input == "scissors" && computer-choice == "paper")
       return true
+    elsif (user_input == "scissors" && computer_choice == "paper")
       puts "You win!"
+      return true
     elsif (user_input == "paper" && computer_choice == "rock")
-      return true
       puts "You win!"
+      return true
     else
-      return false
       puts "Computer wins!"
+      return false
     end
   end
 end
 
+game = RPS.new()
+user_input = game.player_turn
+computer_choice = game.computer_turn
+game.wins?(user_input, computer_choice)
 # public def beats?(arg)
 #   if (self == "rock" && arg == "scissors")
 #     return true
--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -6,4 +6,24 @@ describe('#beats') do
   game = RPS.new()
   expect(game.wins?("rock", "scissors")).to(eq(true))
   end
+  it("returns false if scissors is the object and rock is the argument") do
+  game = RPS.new()
+  expect(game.wins?("scissors", "rock")).to(eq(false))
+  end
+  it("returns true if scissors is the object and paper is the argument") do
+  game = RPS.new()
+  expect(game.wins?("scissors", "paper")).to(eq(true))
+  end
+  it("returns false if paper is the object and scissors is the false") do
+  game = RPS.new()
+  expect(game.wins?("paper", "scissors")).to(eq(false))
+  end
+  it("returns true if paper is the object and rock is the argument") do
+  game = RPS.new()
+  expect(game.wins?("paper", "rock")).to(eq(true))
+  end
+  it("returns true if rock is the object and paper is the argument") do
+  game = RPS.new()
+  expect(game.wins?("rock", "paper")).to(eq(false))
+  end
 end
